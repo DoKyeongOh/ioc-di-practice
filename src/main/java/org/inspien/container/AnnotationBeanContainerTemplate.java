81ee02c54cf63b3c72802ee4da010bc063f5c159
DoKyeong Oh <dkproh@gmail.com>
2023-01-04 17:46:25 +0900

    feat : AnnotationBeanContainerTemplate 추가
    어노테이션을 사용해 빈 컨테이너를 만들기 위한 클래스 추가

---
+++ src/main/java/org/inspien/container/AnnotationBeanContainerTemplate.java
@@ -0,0 +1,97 @@
+package org.inspien.container;
+
+import org.inspien.annotation.Run;
+import org.inspien.annotation.Autowired;
+import org.inspien.annotation.Component;
+import org.reflections.Reflections;
+import org.reflections.scanners.SubTypesScanner;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+public class AnnotationBeanContainerTemplate extends BeanContainerTemplate {
+    @Override
+    protected void init(String... basePackages) {
+        for (String packageName : basePackages) {
+            Reflections reflections = new Reflections(
+                    packageName,
+                    new SubTypesScanner(false)
+            );
+
+            Set<Class<?>> classSet = reflections.getSubTypesOf(Object.class);
+            nameClassMap = getClassWithComponent(classSet);
+        }
+    }
+
+    @Override
+    protected void createInstances() {
+        for (Class aClass : nameClassMap.values()) {
+            try {
+                beanContainer.put(aClass.getName(), aClass.getDeclaredConstructor().newInstance());
+            } catch (InstantiationException e) {
+                throw new RuntimeException(e);
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            } catch (InvocationTargetException e) {
+                throw new RuntimeException(e);
+            } catch (NoSuchMethodException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    protected void injectBean() {
+        for (Object bean : beanContainer.values()) {
+            for (Field field : bean.getClass().getDeclaredFields()) {
+                if (!field.isAnnotationPresent(Autowired.class)) {
+                    continue;
+                }
+                String fieldName = field.getType().getName();
+                if (!beanContainer.containsKey(fieldName)) {
+                    throw new RuntimeException("Injection fail, object "
+                            + field + " in " + bean.getClass() + " is not in bean container!");
+                }
+                try {
+                    field.set(bean, beanContainer.get(fieldName));
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void executeMethods() {
+        for (Object bean : beanContainer.values()) {
+            for (Method method : bean.getClass().getDeclaredMethods()) {
+                if (!method.isAnnotationPresent(Run.class)) {
+                    continue;
+                }
+                try {
+                    method.invoke(bean);
+                } catch (IllegalAccessException e) {
+                    throw new RuntimeException(e);
+                } catch (InvocationTargetException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public Map<String, Class> getClassWithComponent(Set<Class<?>> classSet) {
+        Map<String, Class> classMap = new HashMap<>();
+        for (Class aClass : classSet) {
+            for (Annotation annotation : aClass.getAnnotations()) {
+                if (annotation.annotationType().equals(Component.class)) {
+                    classMap.put(aClass.getName(), aClass);
+                    break;
+                }
+            }
+        }
+        return classMap;
+    }
+}
