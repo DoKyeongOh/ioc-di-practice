6eed0a0e205515d26ee236f171ba14e29525b01e
DoKyeong Oh <dkproh@gmail.com>
2023-01-04 17:48:35 +0900

    feat : 빈 컨테이너 실행 시 사용할 컴포넌트 2개 추가

---
+++ src/main/java/org/inspien/app/MyFirstComponent.java
@@ -0,0 +1,10 @@
+package org.inspien.app;
+
+import org.inspien.annotation.Component;
+
+@Component
+public class MyFirstComponent {
+    public void print() {
+        System.out.println("This is my first component.");
+    }
+}
---
+++ src/main/java/org/inspien/app/MySecondComponent.java
@@ -0,0 +1,10 @@
+package org.inspien.app;
+
+import org.inspien.annotation.Component;
+
+@Component
+public class MySecondComponent {
+    public void print() {
+        System.out.println("This is my second component.");
+    };
+}
